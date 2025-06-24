aa79643019b5e7f0b855a1ac0b46e4a9a0c36f76
Thibault Bondetti <thibault.bondetti@cgi.com>
2025-06-24 10:34:12 +0200

    correction

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Exercice1 from './components/exercice-1/Exercice1';
+import Exercice1 from './components/exercices/exercice-1/Exercice1';
 
 const App = () => (
   <main>
